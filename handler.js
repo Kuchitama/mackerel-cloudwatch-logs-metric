2e08a42362b52067e6d42c2a7edb162f6a8d6f28
Kuchitama <kiyo.kunihira@gmail.com>
2019-12-07 02:56:33 +0900

    Throw avarage of initialDuration to service metric

--- handler.js
+++ handler.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const log4js = require("log4js");
 const moment = require("moment");
 const aws = require("aws-sdk");
+const shell = require("shelljs");
+
+const log4js = require("log4js");
 log4js.configure({
   appenders: { out: { type: 'stdout', layout: { type: 'basic' } } },
   categories: { default: { appenders: ['out'], level: 'info' } },
@@ -10,32 +12,91 @@ log4js.configure({
 });
 const logger = log4js.getLogger();
 
+const MACKEREL_APIKEY = process.env.MACKEREL_APIKEY;
+const MACKEREL_SERVICE = process.env.MACKEREL_SERVICE;
+const DIR = process.env.LAMBDA_TASK_ROOT;
+const logGroupNames = process.env.LOG_GROUPS.split(",");
 
 const cloudwatchLogs = new aws.CloudWatchLogs();
-const MACKEREL_APIKEY = process.env.MACKEREL_APIKEY;
-const logGroupName = process.env.LOG_GROUP;
+const dynamo = new aws.DynamoDB.DocumentClient()
+
+const TableName = "queryTable";
+
+const handleError = (err) => logger.error(err);
+
+const insertDb = async (queryId, now) => {
+  const params = {
+    TableName,
+    Item: {
+      queryId,
+      createdAt: now.unix()
+    }
+  };
+  dynamo.put(params).promise().catch(handleError)
+};
+
+const mkrThrow = (value, epoch) => {
+  const command = `echo "custom.lambda.duration.initAvg\t${value}\t${epoch}" | MACKEREL_APIKEY=${MACKEREL_APIKEY} ${DIR}/mkr throw --service ${MACKEREL_SERVICE}`;
+  return shell.exec(command);
+}
 
 module.exports.queue = async event => {
 
-  logger.info(`APIKEY = ${MACKEREL_APIKEY}, LOG_GROUP = ${logGroupName}`);
   const now = moment();
-  const start = now.subtract(5, "minutes");
+  const start = now.clone().subtract(5, "minutes");
   const queryString = `filter @type = "REPORT" |
   parse @message  "Init Duration: * ms" as initDuration |
-  filter initDuration > 0 |
-  fields @timestamp, @requestId, @billedDuration |
-  stats avg(initDuration)
-  `;
+  stats avg(initDuration) as avg`;
 
-  await cloudwatchLogs.startQuery({
+  const opt = {
     startTime: start.unix(),
     endTime: now.unix(),
     queryString,
-    logGroupName
-  })
+    logGroupNames
+  };
+  const { queryId } = await cloudwatchLogs.startQuery(opt)
   .promise()
-  .then(res => logger.info(res))
-  .catch(err => logger.error(err))
+  .catch(handleError)
 
-  return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  await insertDb(queryId, now)
+
+  const message = `started queryId: ${queryId}`;
+  logger.info(message)
+  return { message, event };
+};
+
+module.exports.dequeue = async event => {
+  const queries = await dynamo.scan({ TableName }).promise().then(res => {
+    logger.info(`get ${res.Items.length} records`);
+    return res.Items.sort((a, b) => a.createdAt - b.createdAt);
+  }).catch(handleError);
+
+  if (queries.length === 0) { return { message: `No recodes.`, event }; }
+
+  const lastQuery = queries.slice(-1)[0];
+  await Promise.all(queries.map((q) => dynamo.delete({ TableName, Key: { queryId: q.queryId }}).promise()));
+
+  const result = await cloudwatchLogs.getQueryResults({queryId: lastQuery.queryId}).promise()
+    .then((res) => {
+      const { status, results } = res;
+      logger.info(`${status}: ${lastQuery.queryId}`);
+      if (status === "Complete") {
+        logger.info(results);
+        if (results.length !== 0 && results[0].length !== 0) {
+          return mkrThrow(results[0][0].value, lastQuery.createdAt);
+        } else {
+          return mkrThrow(0, lastQuery.createdAt);
+        }
+      } if (status === "Scheduled" || status === "Running") {
+        // Re-insert to DynamoDB
+        insertDb(lastQuery.queryId, lastQuery.createdAt);
+      }
+    })
+    .catch(handleError);
+
+  if (result.code !== 0) {
+    logger.error(result.stderr);
+  }
+
+  return { message: `compolete processes of ${lastQuery}`, event };
 };
